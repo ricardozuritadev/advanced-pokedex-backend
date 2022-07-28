3f98bb1ff38ce87952b9b4483cf2b29f9a465578
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-07-28 12:50:40 +0200

    chore: trainer profile route created

--- services/index.js
+++ services/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 module.exports = db => {
   router.use('/auth', require('./auth')(db));
+  router.use('/trainer-profile', require('./trainer')(db));
 
   return router;
 };
