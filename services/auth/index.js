509a5f4681653e1a0f547e8127b5c0da45cc4cc4
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-07-28 11:18:55 +0200

    chore: checker middleware implemented

--- services/auth/index.js
+++ services/auth/index.js
@@ -1,9 +1,14 @@
 const router = require('express').Router();
-const authorizer = require('../../middlewares/authorizer');
+const forms = require('../../utils/forms-fields');
+const { authorizer, checker } = require('../../middlewares');
 
 module.exports = db => {
-  router.post('/register', require('./register')(db));
-  router.post('/login', require('./login')(db));
+  router.post(
+    '/register',
+    checker(...forms.register),
+    require('./register')(db)
+  );
+  router.post('/login', checker(...forms.login), require('./login')(db));
   router.post('/logout', authorizer, require('./logout')(db));
 
   return router;
