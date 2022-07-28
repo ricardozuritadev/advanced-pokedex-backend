990c05b912856237080283de4ed7ab3d25dc98c8
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-07-28 10:44:39 +0200

    chore: authorization middleware implemented in logout

--- services/auth/index.js
+++ services/auth/index.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
+const authorizer = require('../../middlewares/authorizer');
 
 module.exports = db => {
   router.post('/register', require('./register')(db));
   router.post('/login', require('./login')(db));
-  router.post('/logout', require('./logout')(db));
+  router.post('/logout', authorizer, require('./logout')(db));
 
   return router;
 };
