b5dbca5077186ba483b0707cf32bed6583b4d084
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-07-28 13:27:12 +0200

    chore: send register email implemented

--- services/auth/register.js
+++ services/auth/register.js
@@ -1,7 +1,7 @@
 // MODULES
 const { createTrainer } = require('../../queries/auth');
+const { hash, mailer } = require('../../utils');
 const { register } = require('../../errors/auth');
-const { encrypt } = require('../../utils/hash');
 const errors = require('../../errors/commons');
 
 module.exports = db => async (req, res, next) => {
@@ -12,12 +12,15 @@ module.exports = db => async (req, res, next) => {
   const queryResult = await createTrainer(db)({
     email,
     nickname,
-    password: await encrypt(password),
+    password: await hash.encrypt(password),
   });
 
   // If ok property is false, return error with code property
   // If there's another kind of error, like some misspelled word, return 500 error
   if (!queryResult.ok) return next(register[queryResult.code] || errors[500]);
+
+  // Send welcome message to user email
+  await mailer.send({ to: email, type: 'welcome' });
 
   // If ok is true, return data to client
   res.status(200).json({
