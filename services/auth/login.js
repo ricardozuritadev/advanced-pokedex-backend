bf4989f517818994352aeb904c741539c07eb43a
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-07-28 11:19:20 +0200

    chore: login controller refactorized

--- services/auth/login.js
+++ services/auth/login.js
@@ -1,22 +1,25 @@
 const { getCorrectTrainer } = require('../../queries/auth');
 const { hash, serialize } = require('../../utils');
-const { generic, login } = require('../../errors/auth');
+const { login } = require('../../errors/auth');
 const errors = require('../../errors/commons');
 
 module.exports = db => async (req, res, next) => {
+  // Read data from body
   const { nickname, password } = req.body;
 
-  if (!nickname || !password) return next(generic['empty']);
-
+  // If everithing is ok, get correct trainer from DB
   const queryResult = await getCorrectTrainer(db)({
     nickname,
     compareFn: hash.compare(password),
   });
 
+  // If ok properti is false, return login error or internal server error
   if (!queryResult.ok) return next(login[queryResult.code] || errors[500]);
 
+  // Serialize token
   serialize(res, { nickname: queryResult.data.nickname });
 
+  // Send success response to client
   res.status(200).json({
     success: true,
   });
