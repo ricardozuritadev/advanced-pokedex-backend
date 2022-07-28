fd9944e83e65e995d105d56be5e27105bf0b142c
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-07-28 10:09:48 +0200

    chore: logout route implemented

--- services/auth/index.js
+++ services/auth/index.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 module.exports = db => {
   router.post('/register', require('./register')(db));
   router.post('/login', require('./login')(db));
+  router.post('/logout', require('./logout')(db));
 
   return router;
 };
