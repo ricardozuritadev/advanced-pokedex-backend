9907446936c59e4f12da89c9acdf545bee41c73d
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-07-28 10:10:45 +0200

    chore: verify function created

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,10 +1,21 @@
 const jwt = require('jsonwebtoken');
 
-// sign function
+// sign token function
 const sign = payload => {
   return jwt.sign(payload, process.env.SECRET);
 };
 
+// verify token function
+const verify = token => {
+  try {
+    return jwt.verify(token, process.env.SECRET);
+  } catch (error) {
+    console.log('> [verify]: ', error.message);
+    return false;
+  }
+};
+
 module.exports = {
   sign,
+  verify,
 };
