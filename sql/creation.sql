e2be0750d0bac6b90362d03e4e66e4ad24b572c3
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-07-29 15:29:46 +0200

    chore: sql file modified

--- sql/creation.sql
+++ sql/creation.sql
@@ -4,5 +4,7 @@ CREATE TABLE IF NOT EXISTS trainers (
     id SERIAL PRIMARY KEY,  
     email TEXT UNIQUE NOT NULL,
     nickname TEXT UNIQUE NOT NULL,
-    password TEXT NOT NULL
+    gendre TEXT,
+    password TEXT NOT NULL,
+    password_confirm TEXT NOT NULL
 );
