09c836ef34fd9723bf9d9ba2dd9d4d61a11278be
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-08-03 12:37:26 +0200

    chore: cors options modified

--- configs/cors.js
+++ configs/cors.js
@@ -4,17 +4,25 @@
 //  'http://localhost:3000',
 //];
 
-const whitelist = [
-  'https://advanced-pokedex-frontend.vercel.app/',
-];
+// const whitelist = [
+//   'https://advanced-pokedex-frontend.vercel.app/',
+// ];
+
+// module.exports = {
+//   origin: (origin, callback) => {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error());
+//     }
+//   },
+//   credentials: true,
+// };
 
 module.exports = {
-  origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error());
-    }
-  },
+  origin: 'https://advanced-pokedex-frontend.vercel.app/',
+  methods: ['GET', 'PUT', 'POST', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
+  preflightContinue: false,
 };
--- index.js
+++ index.js
@@ -12,28 +12,8 @@ const app = express();
 // MIDDLEWARES
 // Body parser, cookie parser & cors
 app.use(cors(options));
-// app.use(express.json());
-// app.use(cookieParser());
-
-//CORS middleware
-var allowCrossDomain = (_, res, next) => {
-  res.header(
-    'Access-Control-Allow-Origin',
-    'https://advanced-pokedex-frontend.vercel.app/'
-  );
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-
-  next();
-};
-//...
-app.configure(() => {
-  app.use(express.json());
-  app.use(express.cookieParser());
-  app.use(express.methodOverride());
-  app.use(allowCrossDomain);
-  // app.use(app.router);
-});
+app.use(express.json());
+app.use(cookieParser());
 // Routes
 app.use('/api/v1', require('./services')(db));
 // Undefined routes
